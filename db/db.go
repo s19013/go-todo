bba0742b663144b047ea12c6a33e12bd3c4a5b32
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-28 23:15:33 +0900

    データベースと接続できるように設定

--- db/db.go
+++ db/db.go
@@ -3,30 +3,51 @@ package db
 import (
 	"database/sql"
 	_ "embed"
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// NewDB returns go-sqlite3 driver based *sql.DB.
-func NewDB(path string) (*sql.DB, error) {
-	db, err := sql.Open("sqlite3", path)
+// これで同じディレクトリ内のschema.sqlが読み込まれるらしい
+//
+//go:embed schema.sql
+var schema string
+
+// fatalで止めるのでerrorを返す必要はない
+func NewDB() *sql.DB {
+	// envファイル読み込み
+	err := godotenv.Load(".env")
 	if err != nil {
-		return nil, err
+		log.Fatalf("環境変数を読み込めませんでした: %v", err)
 	}
 
-	// SQLファイルを読み込む
-	// ioutilは古くて非推奨らしいのでosを使う
-	schema, err := os.ReadFile("schema.sql")
+	// データベースへのパス?
+	var dbpath string
+	if os.Getenv("DB_CONNECTION") == "sqlite3" {
+		dbpath = os.Getenv("DB_PATH")
+	} else {
+		dbpath = fmt.Sprintf(
+			"%v:%v@tcp(%v:%v)/%v",
+			os.Getenv("DB_USERNAME"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_DATABASE"),
+		)
+	}
+
+	db, err := sql.Open(os.Getenv("DB_CONNECTION"), dbpath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("データベースに接続できませんでした: %v", err)
 	}
 
 	// スキーマ実行
-	if _, err := db.Exec(string(schema)); err != nil {
-		return nil, err
+	if _, err := db.Exec(schema); err != nil {
+		log.Fatalf("スキーマを実行できませんでした: %v", err)
 	}
 
-	return db, nil
+	return db
 }
