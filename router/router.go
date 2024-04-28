0e30ee9921a781deb263aad5a940efc183a1128b
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-28 23:38:24 +0900

    テストとしてhelloworldという文字列が返ってくるように設定

--- router/router.go
+++ router/router.go
@@ -2,11 +2,14 @@ package router
 
 import (
 	"database/sql"
+	"go-todo/handler"
 	"net/http"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/test", handler.NewHelloWorldHandler().ServeHTTP)
 
 	return mux
 }
