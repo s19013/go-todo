9301fa99587476d62649a2fa1614ebc1df93e9a6
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-29 21:24:05 +0900

    "/todo"をpostで受け取ったとき何とつなげるのか設定

--- router/router.go
+++ router/router.go
@@ -3,13 +3,21 @@ package router
 import (
 	"database/sql"
 	"go-todo/handler"
+	"go-todo/service"
 	"net/http"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/test", handler.NewHelloWorldHandler().ServeHTTP)
+	mux.HandleFunc("/todo", func(writer http.ResponseWriter, request *http.Request) {
+		todoService := service.NewTODOService(todoDB)
+		todoHandler := handler.NewTODOHandler(todoService)
+
+		if request.Method == http.MethodPost {
+			todoHandler.Create(writer, request)
+		}
+	})
 
 	return mux
 }
