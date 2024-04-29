d3217732abe5b382058abbf761592928a7865a32
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-30 00:39:59 +0900

    getのハンドラーを書いた

--- handler/todo.go
+++ handler/todo.go
@@ -5,7 +5,10 @@ import (
 	"go-todo/model"
 	"go-todo/service"
 	"io"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type TodoHandler struct {
@@ -56,6 +59,60 @@ func (handler *TodoHandler) Create(writer http.ResponseWriter, request *http.Req
 	writer.Header().Set("Content-Type", "application/json")
 	err4 := json.NewEncoder(writer).Encode(response)
 	if err4 != nil {
+		errorResponse.AddErrorMessage("server error")
+		errorResponse.CreateErrorResponse(writer, http.StatusInternalServerError)
+		return
+	}
+}
+
+func (handler *TodoHandler) Get(writer http.ResponseWriter, request *http.Request) {
+	errorResponse := model.NewErrorMessages()
+
+	// パラメータを取得する関数がないので自分でどうにかするしかない
+	// URLのPath部分を"/"で分割
+	parts := strings.Split(request.URL.Path, "/")
+
+	// 先にidパラメーターが空文字どうかを確認
+	// [,"todo",id]の3つの要素が入った配列が帰ってくる｡
+	// "/todo/"だとidは空文字になる
+	// "/todo/1"だとidは1になる
+	id := parts[len(parts)-1]
+
+	if id == "" {
+		log.Println("Error:id not found")
+		errorResponse.AddErrorMessage("id not found")
+		errorResponse.CreateErrorResponse(writer, http.StatusBadRequest)
+		return
+	}
+
+	// 今は文字列のままなので数値型に変換する必要がある
+	intId, err := strconv.Atoi(id)
+	if err != nil {
+		log.Printf("Error:%v\n", err)
+		errorResponse.AddErrorMessage("server error")
+		errorResponse.CreateErrorResponse(writer, http.StatusInternalServerError)
+		return
+	}
+
+	// note:ぶっちゃけわざわざリクエスト型にいれる必要があるかどうかは疑問
+	// まあ､今後パラメータ増えるかも?だし｡
+	todoRequest := model.GetTodoRequest{
+		ID: intId,
+	}
+
+	// todoを受け取る
+	response, err3 := handler.service.Get(request.Context(), todoRequest)
+	if err3 != nil {
+		log.Printf("Error:%v\n", err)
+		errorResponse.AddErrorMessage("server error")
+		errorResponse.CreateErrorResponse(writer, http.StatusInternalServerError)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	err4 := json.NewEncoder(writer).Encode(response)
+	if err4 != nil {
+		log.Printf("Error:%v\n", err)
 		errorResponse.AddErrorMessage("server error")
 		errorResponse.CreateErrorResponse(writer, http.StatusInternalServerError)
 		return
